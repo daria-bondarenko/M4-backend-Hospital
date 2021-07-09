f6170f691734ff18105fda0908520dc5b50c76c0
daria-bondarenko <dbondarenko.exceedteam@gmail.com>
2021-07-09 11:42:44 +0300

    add getAllUsers request

--- src/modules/controllers/record.controller.js
+++ src/modules/controllers/record.controller.js
@@ -2,7 +2,8 @@ const Record = require('../../db/models/record/index');
 const jwtDecode = require('jwt-decode');
 
 module.exports.getAllRecords = async (req, res) => {
-
+  const result = await Record.find({userId: req.user._id});
+  res.send({data: result});
 };
 
 module.exports.createNewRecord = (req, res) => {
