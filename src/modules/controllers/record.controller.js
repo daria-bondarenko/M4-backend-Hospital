1c5f3d7b58b5ce14c7ef4fe73e5e36107bab2b38
daria-bondarenko <dbondarenko.exceedteam@gmail.com>
2021-07-09 11:57:58 +0300

    add editRecord request

--- src/modules/controllers/record.controller.js
+++ src/modules/controllers/record.controller.js
@@ -22,7 +22,32 @@ module.exports.createNewRecord = (req, res) => {
 };
 
 module.exports.editRecord = (req, res) => {
+  const {_id} = req.body;
 
+  if ((req.body.hasOwnProperty('_id')
+    && (req.body.hasOwnProperty('name')
+      || req.body.hasOwnProperty('doctor')
+      || req.body.hasOwnProperty('date')
+      || req.body.hasOwnProperty('complaint')))) {
+    for (let key in req.body) {
+      if ((key === 'name'
+        || key === 'doctor'
+        || key === 'date'
+        || key === 'complaint')
+        && (req.body[key])) {
+        Record.updateOne({_id}, {[key]: req.body[key]}
+        ).then(result => {
+          Record.find({userId: req.user._id}).then(result => {
+            res.send({data: result})
+          })
+        })
+      }
+    }
+  } else {
+    Record.find({userId: req.user._id}).then(result => {
+      res.send({data: result})
+    })
+  }
 };
 
 module.exports.deleteRecord = (req, res) => {
